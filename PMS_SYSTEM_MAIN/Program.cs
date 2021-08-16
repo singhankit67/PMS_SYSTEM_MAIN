044bd55823ff3306a663ff6081ed946da31186c3
Ankit Ashok Kumar Singh <singhanki67@gmail.com>
2021-08-16 10:32:22 +0530

    Add project files.

---
+++ PMS_SYSTEM_MAIN/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PMS_SYSTEM_MAIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
